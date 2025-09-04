8ec2eb2a4f0adc8d19014a08627e5cdca500b2b3
sahil-mondal07 <mondalsahil591@gmail.com>
2025-09-04 19:57:25 +0530

    code 8

--- Q8_Code.c
+++ Q8_Code.c
@@ -3,6 +3,19 @@
 #include <stdio.h>
 
 int main(){
-    
+    int n, i, sum = 0;
+
+    printf("enter number : ");
+    scanf("%d", &n);
+
+    if (n <= 0){
+        printf("enter correct number");
+    }else{
+        for(i=1; i<=n; i++){
+            sum = sum+i;
+        }
+        printf("the sum of first %d natural numbers are : %d\n", n, sum);
+    }
+
     return 0;
 }

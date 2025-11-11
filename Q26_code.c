3c0766a703f3e0a0853be5bb495e6aacee9aaa44
sahil-mondal07 <mondalsahil591@gmail.com>
2025-11-11 22:50:47 +0530

    code 26

---
+++ Q26_code.c
@@ -0,0 +1,15 @@
+//Write a program to print numbers from 1 to n
+
+#include <stdio.h>
+
+int main(){
+    int n,i;
+    printf("enter n numbers to be printed : ");
+    scanf("%d", &n);
+    for (i=1;i<=n;i++)
+    {
+        printf("%d ", i);
+    }
+    
+    return 0;
+}

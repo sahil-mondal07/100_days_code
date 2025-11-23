5c841ff93a068ea8b67a059c759d7eb17884a07a
sahil-mondal07 <mondalsahil591@gmail.com>
2025-11-23 22:27:12 +0530

    code 52

---
+++ Q52_code.c
@@ -0,0 +1,32 @@
+/*  Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*    */
+
+#include <stdio.h>
+
+int main() {
+    int rows = 10;
+    
+    for (int i = 1; i <= rows; i++) {
+        printf("*\n"); 
+    }
+    
+    return 0;
+}
+

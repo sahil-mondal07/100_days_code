850d9d6e1f26f5a8e0630308af8655ab4e4c83f2
sahil-mondal07 <mondalsahil591@gmail.com>
2025-11-30 17:06:20 +0530

    code 71

---
+++ Q71_code.c
@@ -0,0 +1,35 @@
+//Read and print a matrix.
+
+#include <stdio.h>
+int main()
+{
+    int rows, cols, i, j;
+
+    printf("Enter the number of rows in the matrix: ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns in the matrix: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols];
+
+    printf("Enter the elements of the matrix:\n");
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < cols; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("The matrix is:\n");
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < cols; j++)
+        {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

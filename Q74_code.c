442328187af541b6709386fc7f406c647186817e
sahil-mondal07 <mondalsahil591@gmail.com>
2025-11-30 17:40:08 +0530

    code 74

---
+++ Q74_code.c
@@ -0,0 +1,45 @@
+//Find the transpose of a matrix.
+
+#include <stdio.h>
+int main()
+{
+    int rows, cols, i, j;
+
+    printf("Enter the number of rows in the matrix: ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns in the matrix: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols];
+    int transpose[cols][rows];
+
+    printf("Enter the elements of the matrix:\n");
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < cols; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < cols; j++)
+        {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+
+    printf("The transpose of the matrix is:\n");
+    for (i = 0; i < cols; i++)
+    {
+        for (j = 0; j < rows; j++)
+        {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

f98ac717f5dabae8308d3d4be67d554b5627a865
sahil-mondal07 <mondalsahil591@gmail.com>
2025-11-30 16:37:58 +0530

    code 63

---
+++ Q63_code.c
@@ -0,0 +1,48 @@
+//Merge two arrays.
+
+#include <stdio.h>
+int main()
+{
+    int n1, n2, i, j;
+
+    printf("Enter the number of elements in the first array: ");
+    scanf("%d", &n1);
+    int arr1[n1];
+
+    printf("Enter %d elements for the first array:\n", n1);
+    for (i = 0; i < n1; i++)
+    {
+        scanf("%d", &arr1[i]);
+    }
+
+    printf("Enter the number of elements in the second array: ");
+    scanf("%d", &n2);
+    int arr2[n2];
+
+    printf("Enter %d elements for the second array:\n", n2);
+    for (i = 0; i < n2; i++)
+    {
+        scanf("%d", &arr2[i]);
+    }
+
+    int mergedArray[n1 + n2];
+
+    
+    for (i = 0; i < n1; i++)
+    {
+        mergedArray[i] = arr1[i];
+    }
+    for (j = 0; j < n2; j++)
+    {
+        mergedArray[i + j] = arr2[j];
+    }
+
+    printf("Merged array:\n");
+    for (i = 0; i < n1 + n2; i++)
+    {
+        printf("%d ", mergedArray[i]);
+    }
+    printf("\n");
+
+    return 0;
+}

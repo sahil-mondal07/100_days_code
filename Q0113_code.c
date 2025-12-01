5d0b486f6f9877bec121dde887989ceb3bdbef5e
sahil-mondal07 <mondalsahil591@gmail.com>
2025-12-01 19:33:45 +0530

    code 113

---
+++ Q0113_code.c
@@ -0,0 +1,45 @@
+/*Write a program to take an integer array arr and an integer k as inputs. The task is to find the kth smallest element in the array. Print the kth smallest element as output*/
+
+#include <stdio.h>
+void sortArray(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j+1]
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+int main()
+{
+    int n, k, i;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the value of k: ");
+    scanf("%d", &k);
+
+    // Sort the array
+    sortArray(arr, n);
+
+    if (k > 0 && k <= n) {
+        printf("The %dth smallest element is: %d\n", k, arr[k - 1]);
+    } else {
+        printf("Invalid value of k\n");
+    }
+
+    return 0;
+}

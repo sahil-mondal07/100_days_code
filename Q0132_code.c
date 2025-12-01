a5a29c4a8dfab853edfdb566e51901ce0a395f2e
sahil-mondal07 <mondalsahil591@gmail.com>
2025-12-01 22:23:45 +0530

    code 134

--- Q0132_code.c
+++ Q0132_code.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 
-// Define enum for traffic lights
+
 enum TrafficLight {
     RED,
     YELLOW,
@@ -13,11 +13,11 @@ int main() {
     enum TrafficLight light;
     char input[10];
 
-    // Take input as string
+  
     printf("Enter traffic light color (RED, YELLOW, GREEN): ");
     scanf("%s", input);
 
-    // Map input string to enum value
+   
     if (strcmp(input, "RED") == 0) {
         light = RED;
     } else if (strcmp(input, "YELLOW") == 0) {
@@ -29,7 +29,7 @@ int main() {
         return 1;
     }
 
-    // Print action based on enum value
+    
     switch (light) {
         case RED:
             printf("Stop\n");
---
+++ Q0134_code.c
@@ -0,0 +1,43 @@
+//Define an enum with SUCCESS, FAILURE, and TIMEOUT, and print messages accordingly.
+
+#include <stdio.h>
+#include <string.h>
+
+
+enum Status {
+    SUCCESS,
+    FAILURE,
+    TIMEOUT
+};
+
+int main() {
+    char input[10];
+    printf("Enter status (SUCCESS, FAILURE, TIMEOUT): ");
+    scanf("%s", input);
+
+    enum Status status;
+
+   
+    if (strcmp(input, "SUCCESS") == 0) status = SUCCESS;
+    else if (strcmp(input, "FAILURE") == 0) status = FAILURE;
+    else if (strcmp(input, "TIMEOUT") == 0) status = TIMEOUT;
+    else {
+        printf("Invalid input.\n");
+        return 1;
+    }
+
+    
+    switch (status) {
+        case SUCCESS:
+            printf("Operation succeeded\n");
+            break;
+        case FAILURE:
+            printf("Operation failed\n");
+            break;
+        case TIMEOUT:
+            printf("Operation timed out\n");
+            break;
+    }
+
+    return 0;
+}
